434e11f65c5564080a2752d6c4d336a81b4c713e
MauVal96 <m.valadezolmos@ugto.mx>
2018-12-01 17:43:23 -0600

    Interfaz simple

--- main.cc
+++ main.cc
@@ -8,32 +8,32 @@ int main() {
   fuzzy f;
   double d = 0, v = 0, a = 0; //condiciones iniciales del sistema
   double Theta = 0; //angulo de ajuste del sistema.
-  double u = 0, t = 0; //parametros del sitema: factor de rozamiento y tiempo de accion.
-  double a0 = 0, d0 = 0, v0 = 0; //variables de movimiento, aceleracion instantanea, distancia anterior (i-1) y velocidad anterior (i-1)
+  double u = 0, t = 0.1; //parametros del sitema: factor de rozamiento y tiempo de accion.
+  double d0 = 0, v0 = 0; //variables de movimiento, aceleracion instantanea, distancia anterior (i-1) y velocidad anterior (i-1)
   int it = 0; //iterador
 
   //TEST WITHOUT INTERFAZ
-  d = 5;
+  /*d = 5;
   v = 3;
   t = 0.5;
-  u = 0.1;
+  u = 0.1;*/
 
-  std::cout<<"STARTING SIMULATION"<<std::endl;
-  while( ( d > 0.25 || d < -0.25 ) || ( v > 0.125 || v < -0.125 )  ){
+  std::cout<<"\t\tFuzzy Logic control algorithm \n\n "<<std::endl;
+  std::cout<<"Introduce distance value [-10,10]: ";
+  std::cin>>d;
+  std::cout<<"Introduce distance value [-6,6]: ";
+  std::cin>>v;
+
+  std::cout<<"\nSTARTING SIMULATION"<<std::endl;
+  while( ( d > 0.1 || d < -0.1 ) || ( v > 0.1 || v < -0.1 )  ){
     std::cout<<"Iteration no. "<< it<<std::endl;
     Theta = f.fuzzy_logic(d, v);
     std::cout<<"adjustment: "<< Theta<< " degrees."<<std::endl;
     std::cout<<std::endl;
-    if(u*cos(fabs(Theta*(PI/180))) > sin(fabs(Theta*(PI/180)))){
-        a = 0;
-    }
-    else{
-        a = G*sin(fabs(Theta*(PI/180))) - u*G*cos(fabs(Theta*(PI/180))) + a0;
-    }
+    a = G*sin(fabs(Theta*(PI/180))) - u*G*cos(fabs(Theta*(PI/180)));
     if(Theta > 0) a = a*(-1);
     d0 = d;
     v0 = v;
-    a0 = a;
     v = v0 + a*t;
     d = d0 + v0*t + (0.5)*a*(t*t);
     std::cout<<"aceleration = " << a << std::endl;
@@ -45,7 +45,7 @@ int main() {
   }
   std::cout<<"**********************************************"<<std::endl;
   it++;
-  if(it > 100)
+  if(it > 1000)
   {
     break;
   }
